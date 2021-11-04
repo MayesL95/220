a48febaed3f1b0027698777b1afb9f21ff0af4cb
Lindy Mayes <mayesl@g.cofc.edu>
2021-11-04 13:44:09 -0400

    final version of bumper assignment

--- assignments/hw8/bumper.py
+++ assignments/hw8/bumper.py
@@ -6,54 +6,97 @@ File Name: bumper.py
 Problem: simulate bumper cars with circles
 
 Certification of Authenticity:
-I certify that this assignment is entirely my own work but a tutor tried to help
+I certify that this assignment is entirely my own work but had a tutor's assistance
 """
 
 from graphics import *
 from random import randint
 import time
+import math
 
-def main():
-    win = GraphWin("Bumper Cars Simulation", 500, 500)
-    win.setCoords(0, 0, 10, 10)
-    radius = .5
-    dx, dy = 1, 1
 
-    circle_one_point = Point(randint(0, 10), randint(0, 10))
+def get_random(move_amount):
+    random_move_amount = randint(-move_amount, move_amount)
+    return random_move_amount
+
+
+def did_collide(ball1, ball2):
+    point1 = ball1.getCenter()
+    x1 = point1.getX()
+    y1 = point1.getY()
+
+    point2 = ball2.getCenter()
+    x2 = point2.getX()
+    y2 = point2.getY()
+
+    if (math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) <= (ball1.getRadius() + ball2.getRadius())):
+        return True
+    else:
+        return False
+
+def hit_vertical(ball, win):
+    if (ball.getCenter()).getX() >= 200 or (ball.getCenter()).getX() <= 0:
+        return True
+    else:
+        return False
+
+def hit_horizontal(ball, win):
+    if (ball.getCenter()).getY() >= 200 or (ball.getCenter()).getY() <= 0:
+        return True
+    else:
+        return False
+
+
+def get_random_color():
+    random_color = randint(200, 300)
+    return random_color
+
+
+def create_bumpercar_sim():
+    win = GraphWin("Bumper Cars Simulation", 200, 200)
+    radius = 10
+
+    circle_one_point = Point(20, 20)
     circle_one = Circle(circle_one_point, radius)
-    circle_one.setOutline("blue")
-    circle_one.setFill("blue")
+    circle_one.setOutline("white")
+    circle_one.setFill("red")
     circle_one.draw(win)
 
-    circle_two_point = Point(randint(0, 10), randint(0, 10))
+    circle_two_point = Point(100, 100)
     circle_two = Circle(circle_two_point, radius)
-    circle_two.setOutline("red")
-    circle_two.setFill("red")
+    circle_two.setOutline("white")
+    circle_two.setFill("blue")
     circle_two.draw(win)
 
-    x_bottom = radius
-    x_top = win.getWidth() - radius
-    y_bottom = radius
-    y_top = win.getHeight() - radius
+    move_speed_x1 = get_random(13)
+    move_speed_x2 = get_random(13)
+    move_speed_y1 = get_random(13)
+    move_speed_y2 = get_random(13)
 
-    while True:
-        time.sleep(.5)
-        circle_one.move(dx, dy)
-        circle_two.move(dx, dy)
-        if circle_one.getCenter().getY() <= y_bottom or circle_one.getCenter().getY() >= y_top:
-            dy = -dy
-        elif circle_one.getCenter().getX() <= x_bottom or circle_one.getCenter().getX() >= x_top:
-            dx = -dx
-        elif circle_two.getCenter().getY() <= y_bottom or circle_two.getCenter().getY() >= y_top:
-            dy = -dy
-        elif circle_one.getCenter().getX() <= x_bottom or circle_two.getCenter().getX() >= x_top:
-            dx = -dx
-        elif circle_one.getCenter() == circle_two.getCenter():
-            dx, dy = -dx, -dy
+    while win.checkMouse() == None:
+        if did_collide(circle_one, circle_two) == True:
+            move_speed_x1 = -move_speed_x1
+            move_speed_y1 = -move_speed_y1
+            move_speed_x2 = -move_speed_x2
+            move_speed_y2 = -move_speed_y2
+        if hit_vertical(circle_one, win) == True:
+            move_speed_x1 = -move_speed_x1
+        if hit_vertical(circle_two, win) == True:
+            move_speed_x2 = -move_speed_x2
+        if hit_horizontal(circle_one, win) == True:
+            move_speed_y1 = -move_speed_y1
+        if hit_horizontal(circle_two, win) == True:
+            move_speed_y2 = -move_speed_y2
+        circle_one.move(move_speed_x1, move_speed_y1)
+        circle_two.move(move_speed_x2, move_speed_y2)
 
-
-    win.getMouse()
+        time.sleep(.05)
     win.close()
+
+
+def main():
+    create_bumpercar_sim()
+
 
 if __name__ == '__main__':
     main()
