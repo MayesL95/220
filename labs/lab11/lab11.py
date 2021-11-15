b11f16a6649773341d1801c6894a7ef56eed397e
Lindy Mayes <mayesl@g.cofc.edu>
2021-11-15 14:44:59 -0500

    worked on lab 11, not complete

--- labs/lab11/lab11.py
+++ labs/lab11/lab11.py
@@ -1,11 +1,50 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Lindy Mayes
+hangman.py
 """
+from random import randint
+
+def words (infile):
+    infile = open(infile, "r")
+    content = infile.read()
+    return content.split("\n")
+
+def random_word(list):
+    return list[randint(0, len(list))]
+
+def fill(word, letters):
+    secret = ["_"] * len(word)
+    for letter in letters:
+        for i in range(len(word)):
+            if letter == word[i]:
+                secret[i] = letter
+    return "".join(secret)
+
+def won(word, letters):
+
+
+
+def play():
+    w = words("wordslist.txt")
+    secret = random_word(w)
+    incorrect = 0
+    guesses = []
+    current = "_" * len(secret)
+    while not won(): and while incorrect < 7:
+        display = fill(secret, guesses)
+        print(display)
+        print(guesses)
+        letter = input('guess your letter')
+        while guesses in w:
+            letter = input('guess a letter')
+            guesses.append(letter)
+            display = fill(secret, guesses)
+            if incorrect:
+                incorrect += 1
+            else:
+                current = display
 
 def main():
-    # add other function calls here
-    pass
-
+    play()
 
 main()
